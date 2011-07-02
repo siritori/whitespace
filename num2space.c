8afc5824109dd5c53cd9a99f2c5bdf3c03f44bc3
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2011-07-03 02:02:33 +0900

    再帰使って綺麗にnum2spaceがかけた！嬉しい！ space2numはとりあえず一旦消して、もう一度。 modified:   num2space.c

--- num2space.c
+++ num2space.c
@@ -3,18 +3,33 @@
 #include <stdlib.h>
 #define MAX_NUM_ARRAY_SIZE (sizeof(int) * CHAR_BIT)
 
+int _num2space(int num, char *ret) {
+   int idx;
+   if(num == 1) {
+      printf("[0] = 1\n");
+      ret[0] = '\t';
+      return 1;
+   } else if(num == 0) {
+      printf("[0] = 0\n");
+      ret[0] = ' ';
+      return 1;
+   }
+   idx = _num2space(num/2, ret);
+   printf("[%d] = %d\n", idx, num%2);
+   ret[idx] = (num % 2 == 1)?'\t':' ';
+   return idx+1;
+}
+
 char* num2space(int num) {
-   int i;
-   char *ret = (char*)malloc(MAX_NUM_ARRAY_SIZE);
+   int idx;
+   char *ret = (char*)malloc(sizeof(int) * CHAR_BIT);
    if(ret == NULL) {
-      fprintf(stderr, "FATAL ERROR: failed to allocate memory.\n");
+      fprintf(stderr, "FATAL ERROR: Failed to allocate memory.\n");
       exit(EXIT_FAILURE);
    }
-   for(i = 0; i < MAX_NUM_ARRAY_SIZE; ++i) {
-      ret[(MAX_NUM_ARRAY_SIZE-1) - i] = (num % 2 == 0)?' ':'\t';
-      num /= 2;
-   }
-   ret[(MAX_NUM_ARRAY_SIZE-1) - i] = '\n';
+   idx = _num2space(num, ret);
+   printf("[%d] = '\\n'\n", idx);
+   ret[idx] = '\n';
    return ret;
 }
 
@@ -31,6 +46,7 @@ int main(int argc, char *argv[]) {
          exit(EXIT_FAILURE);
       }
    }
+   free(ret);
 LOOP_END:
    return 0;
 }
