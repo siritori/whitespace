82a73071ed5d239c6b5adaa566637238596ea8a7
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2011-07-02 23:56:09 +0900

    お名前変えました new file:   ws_trans.c deleted:    translator.c

---
+++ ws_trans.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+   char ch;
+   FILE *fp;
+   
+   /* Open file stream */
+   if(argc < 2) { // no input file
+      fp = stdin;
+   } else if(argc == 2) { // file is specified
+      if((fp = fopen(argv[1], "r")) == NULL) {
+         fprintf(stderr, "ERROR: input file is invalid '%s'\n", argv[1]);
+         exit(EXIT_FAILURE);
+      }
+   } else { // illegal arg
+      fprintf(stderr, "Usage: ws_trans [file]\n");
+      fprintf(stderr, "if there're no args, input is stdin.");
+      exit(EXIT_SUCCESS);
+   }
+   /* translation */
+   while((ch = fgetc(fp)) != EOF) {
+      switch(ch) {
+      case 'T': putchar('\t'); break;
+      case 'S': putchar(' '); break;
+      case 'N': putchar('\n'); break;
+      }
+   }
+   fclose(fp);
+   return 0;
+}
+
