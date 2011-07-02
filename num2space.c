f23f22b3077376dbde06d7f7db24103ce9940a24
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2011-07-02 19:29:07 +0900

    10進数から空白文字への変換になりました(>_<*) modified:   num2space.c

--- num2space.c
+++ num2space.c
@@ -11,9 +11,10 @@ char* num2space(int num) {
       exit(EXIT_FAILURE);
    }
    for(i = 0; i < MAX_NUM_ARRAY_SIZE; ++i) {
-      ret[(MAX_NUM_ARRAY_SIZE-1) - i] = num % 2;
+      ret[(MAX_NUM_ARRAY_SIZE-1) - i] = (num % 2 == 0)?' ':'\t';
       num /= 2;
    }
+   ret[(MAX_NUM_ARRAY_SIZE-1) - i] = '\n';
    return ret;
 }
 
@@ -21,8 +22,16 @@ int main(int argc, char *argv[]) {
    int i;
    char *ret = num2space(atoi(argv[1]));
    for(i = 0; i < MAX_NUM_ARRAY_SIZE; ++i) {
-      putchar('0' + ret[i]);
+      switch(ret[i]) {
+      case '\t': putchar('1'); break;
+      case ' ' : putchar('0'); break;
+      case '\n': goto LOOP_END;
+      default  :
+         fprintf(stderr, "FATAL ERROR: Illegal char '%c'\n", ret[i]);
+         exit(EXIT_FAILURE);
+      }
    }
+LOOP_END:
    return 0;
 }
 
