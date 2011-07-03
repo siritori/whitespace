de9be4b1b1401e7782d653520383633168e7a851
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2011-07-03 13:47:01 +0900

    ほかのIMPにも全部対応。全部無事テスト通過しましたぁ: modified:   test.sh

--- test.sh
+++ test.sh
@@ -33,17 +33,44 @@ b[10]='DIV'
 a[11]='TSTT'
 b[11]='MOD'
 
-for i in {1..100}; do
+a[12]='TTS'
+b[12]='PUT'
+
+a[13]='TTT'
+b[13]='GET'
+
+a[13]='NSS'
+b[13]='LBL'
+
+a[14]='NST'
+b[14]='JAL'
+
+a[15]='NSN'
+b[15]='JMP'
+
+a[16]='NTS'
+b[16]='JSZ'
+
+a[17]='NTT'
+b[17]='JSN'
+
+a[18]='NTN'
+b[18]='RET'
+
+a[19]='NNN'
+b[19]='END'
+
+for i in {1..1000}; do
    R=$RANDOM
-   c[$i]=${a[$(( $R % 11 + 1 ))]}
-   d[$i]=${b[$(( $R % 11 + 1 ))]}
+   c[$i]=${a[$(( $R % 18 + 1 ))]}
+   d[$i]=${b[$(( $R % 18 + 1 ))]}
 done
-for i in {1..100}; do
+for i in {1..1000}; do
    echo -n ${c[$i]}
 done
 echo 'NNN' # end of the program
 echo -n '' > expect
-for i in {1..100}; do
+for i in {1..1000}; do
    echo CMD_${d[$i]} >> expect
 done
 echo CMD_END >> expect
