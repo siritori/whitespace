e07515bbc2c190ae578fb2d66a38fe180b7f828d
Eric Sartre <s1111355@coins.tsukuba.ac.jp>
2011-07-07 04:42:55 +0900

    lexerのエラー処理とか順番とかちょっと入れ替えた。ろじっくは全く変えていない
    modified:   lexer.c

--- lexer.c
+++ lexer.c
@@ -22,41 +22,27 @@ static enum IMP_TYPE read_imp(FILE *fp) {
    if(feof(fp)) return IMP_END;
    ch = next_ch(fp);
    switch(ch) {
+   case EOF : return IMP_END;
    case ' ' : return IMP_STACK;
    case '\n': return IMP_FLOW_CTRL;
    case '\t':
       ch = next_ch(fp);
       switch(ch) {
       case ' ' : return IMP_ARITHMETIC;
-      case '\t': return IMP_HEAP_ACCESS;
       case '\n': return IMP_IO;
+      case '\t': return IMP_HEAP_ACCESS;
       default:
-         fprintf(stderr, "0SYNTAX ERROR: (in IMP)expected '\\t' or '\\n' or ' '\n");
+         fprintf(stderr, "SYNTAX ERROR: (in IMP)expected '\\t' or '\\n' or ' '\n");
          exit(EXIT_FAILURE);
       }
-   case EOF:
-      return IMP_END;
-   default:
-      fprintf(stderr, "1SYNTAX ERROR: (in IMP)expected '\\t' or '\\n' or ' '\n");
-      exit(EXIT_FAILURE);
    }
 }
-
 
 static enum CMD_TYPE read_stack_cmd(FILE *fp) {
    char ch;
    ch = next_ch(fp);
    switch(ch) {
    case ' ' : return CMD_PSH;
-   case '\t':
-      ch = next_ch(fp);
-      switch(ch) {
-      case ' ' : return CMD_CPY;
-      case '\n': return CMD_SLD;
-      default:
-         fprintf(stderr, "2SYNTAX ERROR: (in CMD)expected ' ' or '\\n'\n");
-         exit(EXIT_FAILURE);
-      }
    case '\n':
       ch = next_ch(fp);
       switch(ch) {
@@ -64,11 +50,20 @@ static enum CMD_TYPE read_stack_cmd(FILE *fp) {
       case '\n': return CMD_DSC;
       case '\t': return CMD_SWP;
       default:
-         fprintf(stderr, "3SYNTAX ERROR: (in CMD)expected ' ' or '\\n' or '\\t'\n");
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_STACK)expected '\\t' or '\\n' or ' '\n");
+         exit(EXIT_FAILURE);
+      }
+   case '\t':
+      ch = next_ch(fp);
+      switch(ch) {
+      case ' ' : return CMD_CPY;
+      case '\n': return CMD_SLD;
+      default:
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_STACK)expected '\\n' or ' '\n");
          exit(EXIT_FAILURE);
       }
    default:
-      fprintf(stderr, "4SYNTAX ERROR: (in IMP)expected '\\t' or '\\n' or ' '\n");
+      fprintf(stderr, "SYNTAX ERROR: (in IMP_STACK)expected '\\t' or '\\n' or ' '\n");
       exit(EXIT_FAILURE);
    }
 }
@@ -81,10 +76,10 @@ static enum CMD_TYPE read_arithmetic_cmd(FILE *fp) {
       ch = next_ch(fp);
       switch(ch) {
       case ' ' : return CMD_ADD;
-      case '\t': return CMD_SUB;
       case '\n': return CMD_MUL;
+      case '\t': return CMD_SUB;
       default:
-         fprintf(stderr, "5SYNTAX ERROR: (in CMD)expected ' ' or '\\n' or '\\t'\n");
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_ARITHMETIC)expected '\\t' or '\\n' or ' '\n");
          exit(EXIT_FAILURE);
       }
    case '\t':
@@ -93,11 +88,11 @@ static enum CMD_TYPE read_arithmetic_cmd(FILE *fp) {
       case ' ' : return CMD_DIV;
       case '\t': return CMD_MOD;
       default:
-         fprintf(stderr, "6SYNTAX ERROR: (in CMD)expected ' ' or '\\t'\n");
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_ARITHMETIC)expected '\\t' or ' '\n");
          exit(EXIT_FAILURE);
       }
    default:
-      fprintf(stderr, "7SYNTAX ERROR: (in CMD)expected ' ' or '\\t'\n");
+      fprintf(stderr, "SYNTAX ERROR: (in IMP_ARITHMETIC)expected '\\t' or ' '\n");
       exit(EXIT_FAILURE);
    }
 }
@@ -109,7 +104,7 @@ static enum CMD_TYPE read_head_access_cmd(FILE *fp) {
    case ' ' : return CMD_PUT;
    case '\t': return CMD_GET;
    default:
-      fprintf(stderr, "8SYNTAX ERROR: (in CMD)expected ' ' or '\\t'\n");
+      fprintf(stderr, "SYNTAX ERROR: (in IMP_HEAP_ACCESS)expected ' ' or '\\t'\n");
       exit(EXIT_FAILURE);
    }
 }
@@ -118,31 +113,31 @@ static enum CMD_TYPE read_flow_ctrl_cmd(FILE *fp) {
    char ch;
    ch = next_ch(fp);
    switch(ch) {
-   case '\t':
-      ch = next_ch(fp);
-      switch(ch) {
-      case ' ' : return CMD_JSZ;
-      case '\t': return CMD_JSN;
-      case '\n': return CMD_RET;
-      default:
-         fprintf(stderr, "9SYNTAX ERROR: (in CMD)expected ' ' or '\\n' or '\\t'\n");
-         exit(EXIT_FAILURE);
-      }
    case ' ' :
       ch = next_ch(fp);
       switch(ch) {
       case ' ' : return CMD_LBL;
-      case '\t': return CMD_JAL;
       case '\n': return CMD_JMP;
+      case '\t': return CMD_JAL;
       default:
-         fprintf(stderr, "10SYNTAX ERROR: (in CMD)expected ' ' or '\\n' or '\\t'\n");
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_FLOW_CTRL)expected '\\t' or '\\n' or ' '\n");
          exit(EXIT_FAILURE);
       }
    case '\n':
       ch = next_ch(fp);
       if(ch == '\n') return CMD_END;
+   case '\t':
+      ch = next_ch(fp);
+      switch(ch) {
+      case ' ' : return CMD_JSZ;
+      case '\n': return CMD_RET;
+      case '\t': return CMD_JSN;
+      default:
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_FLOW_CTRL)expected '\\t' or '\\n' or ' '\n");
+         exit(EXIT_FAILURE);
+      }
    default:
-      fprintf(stderr, "11 %02X(%02ld) SYNTAX ERROR: (in CMD)expected ' ' or '\\n' or '\\t'\n", ch, ftell(fp));
+      fprintf(stderr, "SYNTAX ERROR: (in IMP_FLOW_CTRL)expected '\\t' or '\\n' or ' '\n");
       exit(EXIT_FAILURE);
    }
 }
@@ -157,7 +152,7 @@ static enum CMD_TYPE read_io_cmd(FILE *fp) {
       case ' ' : return CMD_PCH;
       case '\t': return CMD_PNM;
       default:
-         fprintf(stderr, "12SYNTAX ERROR: (in CMD)expected ' ' or '\\t'\n");
+         fprintf(stderr, "SYNTAX ERROR: (in IMP_IO)expected '\\t' or ' '\n");
          exit(EXIT_FAILURE);
       }
    case '\t':
@@ -166,11 +161,11 @@ static enum CMD_TYPE read_io_cmd(FILE *fp) {
       case ' ' : return CMD_GCH;
       case '\t': return CMD_GNM;
       default:
-         fprintf(stderr, "13SYNTAX ERROR: (in CMD)expected ' ' or '\\t'\n");
+         fprintf(stderr, "SYNTAX ERROR: (in CMD)expected '\\t' or ' '\n");
          exit(EXIT_FAILURE);
       }
    default:
-      fprintf(stderr, "14SYNTAX ERROR: (in CMD)expected ' ' or '\\t'\n");
+      fprintf(stderr, "SYNTAX ERROR: (in CMD)expected '\\t' or ' '\n");
       exit(EXIT_FAILURE);
    }
 }
@@ -192,6 +187,10 @@ static int read_label(FILE *fp) {
    char ch;
    int ret = 0;
    while((ch = next_ch(fp)) != '\n') {
+      if(ch == EOF) {
+         fprintf(stderr, "SYNTAX ERROR: (in label)expected '\\t' or '\\n' or ' '\n");
+         exit(EXIT_FAILURE);
+      }
       ret = (unsigned)(ret * 3 + 7) + ((ch == '\t')? 2 : 5);
       ret %= SYMBOL_TABLE_SIZE;
    }
@@ -204,6 +203,10 @@ static int read_param(FILE *fp) {
    char temp[MAX_NUM_ARRAY_SIZE];
    for(i = 0; i < MAX_NUM_ARRAY_SIZE; ++i) {
       if((ch = next_ch(fp)) == '\n') break;
+      else if(ch == EOF) {
+         fprintf(stderr, "SYNTAX ERROR: (in parameter)expected '\\t' or '\\n' or ' '\n");
+         exit(EXIT_FAILURE);
+      }
       temp[i] = ch;
    }
    temp[i] = ch;
